f32c3353ad77013af621fd17fb7fd21e9d33865d
docia <matiushenko107@gmail.com>
2023-10-09 20:01:59 +0300

    Vite, SASS, ESLint

---
+++ Section-3/app.js
@@ -0,0 +1,14 @@
+let vm = Vue.createApp({
+  
+})
+
+vm.component('hello', {
+  template: `<h1>{{ message }}</h1>`,
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+})
+
+vm.mount('#app')
