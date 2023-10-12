272d28237e5d1a0ffaec4b8e4756597d59350b8e
docia <matiushenko107@gmail.com>
2023-10-12 23:10:50 +0300

    add components

---
+++ Section-6/components/components/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<h3>Hey!</h3>
+<greeting :age="age"></greeting>
+<user :age="age" @age-change="age++"></user>
+</template>
+
+
+<script>
+import Greeting from "./components/Greeting.vue"
+import User from "./components/User.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Greeting,
+    User
+  },
+  data() {
+    return {
+      age: 20
+    }
+  }
+
+}
+</script>
---
+++ Section-6/components/components/src/components/User.vue
@@ -0,0 +1,24 @@
+<template>
+    <button type="button" @click="onClickAge">Update Age</button>
+
+    <p>The user is {{ age }} years old</p>
+</template>
+
+<script >
+export default {
+    name: "User",
+    props: ["age"],
+    methods: {
+        onClickAge() {
+            this.$emit('age-change')
+        }
+    }
+}
+
+
+</script>
+
+<style>
+
+</style>
+
