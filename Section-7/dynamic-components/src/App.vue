001379252c816ea8ac133e2b9208a17dc774e509
docia <matiushenko107@gmail.com>
2023-10-13 15:56:12 +0300

    dynamic components

---
+++ Section-7/dynamic-components/src/App.vue
@@ -0,0 +1,31 @@
+
+<template >
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+
+  <keep-alive>
+    <component :is="componentName"></component>
+  </keep-alive>
+</template>
+
+<script>
+import About from "./components/About.vue"
+import Home from "./components/Home.vue"
+
+export default {
+  name: "App",
+  components: {
+    About, 
+    Home,
+  },
+  data() {
+    return {
+      componentName: "Home",
+    }
+  }
+}
+</script>
+
+
