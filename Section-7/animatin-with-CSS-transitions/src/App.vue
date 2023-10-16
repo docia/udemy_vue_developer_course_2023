04fc6b930ac8ab2b20de2cd7e3f9eb39f60c45f3
docia <matiushenko107@gmail.com>
2023-10-16 15:39:54 +0300

    CSS animations & transitions

--- Section-7/animatin-with-CSS-transitions/src/App.vue
+++ Section-7/animatin-with-CSS-transitions/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
-  <transition name="fade" mode="out-in">
+  <!-- <transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">Hello World</h2>
     <h2 v-else key="secondary">Another hello!</h2>
+  </transition> -->
+
+  <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">Hello</h2>
   </transition>
 </template>
 
@@ -18,33 +22,53 @@ export default {
 </script>
 
 <style>
+h2{
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+  
+}
+
 .fade-enter-from,
-.fade-leave-to {
-  opacity: 0
+.fade-leave-to,
+.zoom-enter-from,
+.zoom-leave-to {
+  opacity: 0;
 }
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: all 3s linear;
+  transition: all 1s linear;
 }
 
-/* .fade-enter-active {
-  animation: bounce-in 0.5s;
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 2s linear;
 }
 
-.fade-leave-active {
-  animation: bounce-in 0.5s reverse;
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 2s linear;
 }
 
-@keyframes bounce-in {
-  0% {
-    transform: scale(0);
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
   }
-  50% {
-    transform: scale(1.25);
+  to {
+    transform: scale(1, 1);
+
   }
-  100% {
-    transform: scale(1);
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
   }
-} */
+  to {
+    transform: scale(0, 0);
+
+  }
+}
+
 </style>
