481efce48e30da3193e99e728ce3fb00f0eea326
docia <matiushenko107@gmail.com>
2023-10-16 14:48:33 +0300

    transition with mode="out-in"

--- Section-7/animatin-with-CSS-transitions/src/App.vue
+++ Section-7/animatin-with-CSS-transitions/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
-  <transition name="fade">
-    <h2 v-if="flag">Hello World</h2>
+  <transition name="fade" mode="out-in">
+    <h2 v-if="flag" key="main">Hello World</h2>
+    <h2 v-else key="secondary">Another hello!</h2>
   </transition>
 </template>
 
@@ -17,16 +18,33 @@ export default {
 </script>
 
 <style>
-.fade-enter-from {
+.fade-enter-from,
+.fade-leave-to {
   opacity: 0
 }
 
-.fade-enter-active {
-  transition: all 0.25s linear;
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 3s linear;
 }
 
-.fade-leave-to {
-  transition: all 0.25s linear;
-  opacity: 0;
+/* .fade-enter-active {
+  animation: bounce-in 0.5s;
 }
+
+.fade-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+} */
 </style>
